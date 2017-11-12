4ba8c4721cc4b5f766d973c8cfb84b240c34d587
Sean D <sdoherty8@icloud.com>
2017-11-12 20:48:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ class BoardMember{
     this.homeState = homeState
     this.training = training
   }
-veto(){}
-approve(){}
-doCharity(){}
-releasePressStatement(){}
-sayHi(){}
+veto(){return `No, I must disagree`}
+approve(){return `You can do that!`}
+doCharity(){return `I like to help people`}
+releasePressStatement(){return `You will see great things from Scuber.`}
+sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 
 
 

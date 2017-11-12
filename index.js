5d82b3ea3ec53cd80f9b67ee38fa29f055910e96
Sean D <sdoherty8@icloud.com>
2017-11-12 20:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ doCharity(){return `I like to help people`}
 releasePressStatement(){return `You will see great things from Scuber.`}
 sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
 
-
-
 }
+
+
+class ceo extends BoardMember

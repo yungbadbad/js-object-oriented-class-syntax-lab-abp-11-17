3caeb8f964d0bf6050ce45aee58e903530c32023
Sean D <sdoherty8@icloud.com>
2017-11-12 20:46:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+class BoardMember{
+  constructor(name, homeState, training){
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+veto(){}
+approve(){}
+doCharity(){}
+releasePressStatement(){}
+sayHi(){}
+
+
+
+}
